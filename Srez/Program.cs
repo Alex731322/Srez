5df17463ebf10743f141f82249c6d0f3cb332a79
Alex <sasha13223@mail.ru>
2020-04-08 14:35:02 +0300

    Добавьте файлы проекта.

---
+++ Srez/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Srez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str =  Console.ReadLine().Split(' ');
+           
+            for (int i = 0; i < str.Length ; i++)
+            {
+                string[] str1 = str[i].Split(',');
+                Console.WriteLine($"X: {str1[0].Replace('.',',')} Y:{str1[1].Replace('.',',')} " );
+            }
+        
+        }
+    }
+}
